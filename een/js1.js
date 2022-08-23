dc10c49b1ff92f5f619dee7c6368cb76a2b329e6
Philip Pretorius <philippretorius.79@gmail.com>
2022-08-23 22:36:00 +0200

    Variables finished

---
+++ een/js1.js
@@ -0,0 +1,38 @@
+console.log("Hello, World!")
+
+
+// Declare variables, define variables and assign values
+
+var a = eggs;
+let b = bacon;
+const c = toast;
+
+// Alert shows variable content
+
+alert(a + b + c);
+
+// Declare multiple variables in one line
+
+    let user = Philip, age = 24, sex = male;
+
+    // Not recommended due to worse readability
+
+// Rather declare multiple variables in multiple lines
+
+    let user2 = Phil,
+        age2 = 24,
+        sex2 = m;
+
+// Copy one variable data to another
+
+let color = orange;
+let fruit;
+fruit = color;
+
+
+
+
+
+
+
+
---
+++ een/variabletasks.js
@@ -0,0 +1,13 @@
+// Variable Tasks
+
+// Task 1
+
+let admin;
+let name = John;
+admin = name;
+alert(admin);
+
+// Task 2
+
+const OurPlanetName = "Earth";
+let CurrentVisitorName = "Jan";
