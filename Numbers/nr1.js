3c443ee4cdc1073fdc82f77af9318ffb9d9c67fe
Philip Pretorius <philippretorius.79@gmail.com>
2022-08-24 13:07:32 +0200

    pituvdeath

--- Numbers/nr1.js
+++ Numbers/nr1.js
@@ -14,3 +14,42 @@
 
     let z = (100 + 50) * c
 
+// Numbers with decimals
+
+    let decimal = 15.50;
+
+    // Period, not comma
+
+// Numbers with scientific notation
+
+    let exponent = 2e10;
+
+// NaN
+
+    let one = 1;
+    let two = NaN;
+    let three = one + two;
+    // Result = NaN
+
+// Infinity
+
+    let myNumber = 2;
+    // Execute until Infinity
+    while (myNumber != Infinity) {
+    myNumber = myNumber * myNumber;
+    }
+
+    // or divide by 0
+
+    let a1 = 2;
+    let a2 = 0;
+    let a3 = a1/a2;
+    // Result = Infinity
+
+// Hexadecimal 
+
+    let hex = 0xFF;
+    
+    // starts with 0x
+
+// JS numbers as objects
