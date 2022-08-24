a5d142e38e07e028afdd468dd533df1621fbc83e
Philip Pretorius <philippretorius.79@gmail.com>
2022-08-24 17:53:49 +0200

    Commit b4 battery quit

--- Numbers/nr1.js
+++ Numbers/nr1.js
@@ -8,11 +8,11 @@
 
     // variables
 
-    let y = a + b;
+    let y = "a" + "b";
 
     // expression
 
-    let z = (100 + 50) * c
+    let z = (100 + 50) * "c"
 
 // Numbers with decimals
 
@@ -30,6 +30,7 @@
     let two = NaN;
     let three = one + two;
     // Result = NaN
+    console.log(three)
 
 // Infinity
 
@@ -38,13 +39,17 @@
     while (myNumber != Infinity) {
     myNumber = myNumber * myNumber;
     }
-
+    // Result
+    console.log(myNumber);
+    
     // or divide by 0
 
     let a1 = 2;
     let a2 = 0;
     let a3 = a1/a2;
+
     // Result = Infinity
+    console.log(a3);
 
 // Hexadecimal 
 
@@ -53,3 +58,73 @@
     // starts with 0x
 
 // JS numbers as objects
+
+    let obj = new Number(500);
+
+    // Result
+    console.log(obj);
+
+// Example from mdn
+
+    const myInt = 5;
+    const myFloat = 6.667;
+    myInt;
+    myFloat;
+    console.log(
+        typeof myInt,
+        typeof myFloat
+    )
+
+
+// Round const decimals
+
+    const lotsOfDecimal = 1.766584958675746364;
+    lotsOfDecimal;
+    const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+    twoDecimalPlaces;
+    console.log(twoDecimalPlaces);
+
+// Converting strings to numbers
+
+    let myString = '74';
+    myString += 3;
+    console.log(myString);
+    // Gives wrong result, 743
+    console.log(typeof myString);
+    // Type of result string
+
+    myString = '74';
+    myString = Number(myString) + 3;
+    console.log(myString);
+
+// Basic Arithmetic 2.0
+
+    console.log(60 % 3);
+    console.log(5 * 2 * 1.5);
+    let num1 = 5;
+    let num2 = 2;
+    console.log(num1 % num2 * num1);
+    console.log((num1 - num2) / 6 * 2);
+
+// Increment and decrement
+
+    num1++;
+    console.log(num1);
+    num2--;
+    console.log(num2);
+    ++num1;
+    console.log(num1);
+    // Updates value with each increment/decrement
+
+// Assignment operators
+
+    let num3 = 4;
+    console.log(num3 += 2);
+    num3 = 4;
+    console.log(num3 -= 2);
+    num3 = 4;
+    console.log(num3 *= 2);
+    num3 = 4;
+    console.log(num3 /= 2);
+
+//
