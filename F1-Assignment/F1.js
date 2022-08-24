dd2318c5b097df2cb39a0e179f029bb2dc25d061
Philip Pretorius <philippretorius.79@gmail.com>
2022-08-24 20:20:40 +0200

    F1 Assignment

---
+++ F1-Assignment/F1.js
@@ -0,0 +1,26 @@
+// 1
+
+console.log(420 + 69)
+
+// 2
+
+console.log(1 + 2 + 3 + 4 + 5 + 6)
+
+// 3
+
+console.log((4 + 6 + 9) / 77)
+
+// 4
+
+let a = 10;
+console.log(a);
+console.log(9 * a)
+let b = 7 * a;
+console.log(b)
+
+// 5
+
+const max = 57;
+let actual = -13;
+let percentage = actual/max;
+console.log(percentage)
