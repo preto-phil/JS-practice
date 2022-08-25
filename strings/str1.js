df60ae29263db03eab077027b1a18881e552f040
Philip Pretorius <philippretorius.79@gmail.com>
2022-08-25 18:14:33 +0200

    save b4 laptop brick

--- strings/str1.js
+++ strings/str1.js
@@ -1,8 +1,33 @@
 //// Strings Practice
 
-// 
+// Create a string
 
-    console.log()
+    const string = 'The revolutiion will not be televised';
+    console.log(string);
+
+    const badString = string;
+    console.log(badString);    
+
+// Single quotes vs double quotes
+
+    const sgl = 'Single quotes.';
+    const dbl = "Double quotes";
+    console.log(sgl);
+    console.log(dbl);
+
+// Why different quotes used 
+
+    const sglDbl = 'Would you eat a "fish supper"?';
+    const dblSgl = "I'm feeling blue.";
+    console.log(sglDbl);
+    console.log(dblSgl);
+
+// Escaping characters in a string
+
+    const bigmouth = 'I\'ve got no right to take my place…';
+    console.log(bigmouth);
+
+// Concatenating strings
 
 
 
@@ -22,9 +47,3 @@
 
 
 
-
-
-
-
-
-    
