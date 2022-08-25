1bb2ad4fd299edf11de28f6f7512cb54f00edde5
Philip Pretorius <philippretorius.79@gmail.com>
2022-08-25 21:37:53 +0200

    another one down

--- strings/str1.js
+++ strings/str1.js
@@ -28,17 +28,59 @@
     console.log(bigmouth);
 
 // Concatenating strings
+    
+    const name = 'Chris';
+    const greeting = `Hello, ${name}`;
+    console.log(greeting); // "Hello, Chris"
 
+// Concatenate using + operator
 
+    const greeting2 = "Hello";
+    const name2 = "John";
+    console.log(greeting2 + ", " + name2); // "Hello, John"
+    
+    // Template literal method
+    
+    console.log(`${greeting2}, ${name2}`)
 
+// Numbers and Strings
 
+    const naam = "Front ";
+    const nommer = 242;
+    console.log(`${naam}${nommer}`); // "Front 242"
 
+    // Number()
+    
+    const myString = '123';
+    const myNum = Number(myString);
+    console.log(typeof myNum);
 
+    // .toString()
 
+    const myNum2 = 123;
+    const myString2 = myNum2.toString();
+    console.log(typeof myString2);
 
+// Expressions
 
+    const song = 'Fight the Youth';
+    const score = 9;
+    const highestScore = 10;
+    const output = `I like the song ${song}. I gave it a score of ${score/highestScore * 100}%.`;
+    console.log(output);  // "I like the song Fight the Youth. I gave it a score of 90%."
 
+// Multiple lines
 
+    const output1 = `I like the song.
+    I gave it a score of 90%.`;
+    console.log(output1);  // I like the song.
+                        // I gave it a score of 90%.
+
+    // \n method
+
+    const output2 = 'I like the song.\nI gave it a score of 90%.';
+    console.log(output2);  // I like the song.
+                        // I gave it a score of 90%.
 
 
 
