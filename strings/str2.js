303181161b1ff6e75a2e432925a5d8269f36e775
Philip Pretorius <philippretorius.79@gmail.com>
2022-08-29 22:36:27 +0200

    String Methods added

---
+++ strings/str2.js
@@ -0,0 +1,13 @@
+// String Methods
+
+// String length
+
+let txt = 'gyggftyfytfytf';
+let length = txt.length;
+console.log(length)
+
+// Extracting string parts
+
+let str = "Apple, Banana, kiwi";
+let part = str.slice(7,13)
+console.log(part)
