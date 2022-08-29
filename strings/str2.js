92a3e79d26ecde236f0a09e20afff2b29e1482a3
Philip Pretorius <philippretorius.79@gmail.com>
2022-08-29 22:57:27 +0200

    SM 1.0

--- strings/str2.js
+++ strings/str2.js
@@ -1,13 +1,42 @@
-// String Methods
+//// String Methods
 
-// String length
+/// String length
 
 let txt = 'gyggftyfytfytf';
 let length = txt.length;
-console.log(length)
+console.log(length);
 
-// Extracting string parts
 
-let str = "Apple, Banana, kiwi";
-let part = str.slice(7,13)
-console.log(part)
+/// Extracting string parts
+
+// Slice
+
+let str = "Apple, Banana, Kiwi";
+let part = str.slice(7,13);
+console.log(part);
+
+// Negative slice
+
+let part2 = str.slice(-12, -6);
+console.log(part2);
+
+// End slice omitted
+
+let part3 = str.slice(15);
+console.log(part3);
+
+// Substring
+
+let part4 = str.substring(7,13);
+console.log(part4);
+
+// Substr
+
+let part5 = str.substr(7, 6);
+console.log(part5)
+
+
+/// Replace
+
+
+
