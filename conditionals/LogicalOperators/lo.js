8b35603e8bfe19c19b7a2d3a543741e4786941ba
Philip Pretorius <philippretorius.79@gmail.com>
2022-08-31 13:18:46 +0200

    OR completed

--- conditionals/LogicalOperators/lo.js
+++ conditionals/LogicalOperators/lo.js
@@ -1,13 +1,58 @@
-//// Logical Operators
-
-// Or ||
+////// Logical Operators
 
 
 
-// And &&
+//// Or ||
+
+// Booleans
+
+alert( true || true );  // true
+alert( false || true );  // true
+alert( true || false );  // true
+alert( false || false ); // false
+
+// Example with if statement
+
+let hour = 9;
+
+if (hour < 10 || hour > 18) {
+    alert('Office is closed.');
+}
+
+// Example with more conditionals
+
+hour = 12;
+let isWeekend = true;
+
+if (hour < 10 || hour > 18 || isWeekend) {
+    alert('Office is closed.');
+}
+
+// Find truthy value
+
+alert( 1 || 0 ); // 1 (1 is truthy)
+
+alert( null || 1 ); // 1 (1 is the first truthy value)
+alert( null || 0 || 1 ); // 1 (the first truthy value)
+
+alert( undefined || null || 0 ); // 0 (all falsy, returns the last value)
+
+// Get truthy value from a list of variables
+
+let firstName = "";
+let lastName = "";
+let nickName = "Stifler";
+
+alert(firstName || lastName || nickName || 'Clarence');
+
+// Short-circuit evaluation
 
 
 
-// Not !
+//// And &&
+
+
+
+//// Not !
 
 
