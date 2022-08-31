1a54171d913eebceea449755fdf3f42c17e9308c
Philip Pretorius <philippretorius.79@gmail.com>
2022-08-31 11:12:44 +0200

    Conditionals 1 Completed

---
+++ conditionals/Conditionals-1/con1.js
@@ -0,0 +1,27 @@
+//// If, Else, Else If
+
+let greeting;
+
+// If Statements
+
+if (hour < 18) {
+    greeting = "Good Day!";
+}
+
+// Else Statement
+
+if (hour < 18) {
+    greeting = "Good Day!"
+} else {
+    greeting = "Good Evening!"
+}
+
+// Else If Statement
+
+if (hour < 10) {
+    greeting = "Good Morning!"
+} else if (hour < 18) {
+    greeting = "Good Day!"
+} else {
+    greeting = "Good Evening!"
+}
