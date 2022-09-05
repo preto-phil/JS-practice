60467bd4f94c486b6237e7252c168dfb955be09f
Philip Pretorius <philippretorius.79@gmail.com>
2022-09-05 17:15:20 +0200

    Some practice

---
+++ F2-Assignment/follow.js
@@ -0,0 +1,12 @@
+/// Follow
+
+let firstName = 'Philip';
+let middleName = 'Geyer';
+let lastName = 'Pretorius';
+
+console.log(`Hi there, my name is ${firstName + " " + middleName + " " + lastName}.`)
+
+let birthYear = 1997;
+let currentYear = 2022;
+
+console.log(`I\'m currently ${currentYear - birthYear} years old.`)
