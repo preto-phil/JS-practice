49b20635518b0e5c495376a7843f4f4c67de8af6
Philip Pretorius <philippretorius.79@gmail.com>
2022-09-05 17:29:49 +0200

    Some additional practice

---
+++ F2-Assignment/math.js
@@ -0,0 +1,18 @@
+//// Maths
+
+let magicNumber = 'three';
+
+console.log(magicNumber);
+
+magicNumber = Number((magicNumber.length));
+
+console.log(magicNumber);
+
+let blazed = 42069;
+
+console.log(blazed);
+
+blazed = blazed.toString();
+
+console.log(blazed);
+
