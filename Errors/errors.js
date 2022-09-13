93c0d15e3b5c8aa926d84ba415bd0f8d3b6a94f8
Philip Pretorius <philippretorius.79@gmail.com>
2022-09-13 15:06:22 +0200

    Added Error Ridden Code

---
+++ Errors/errors.js
@@ -0,0 +1,44 @@
+////// Errors tutorial
+
+//// Reference error
+
+
+let a = 'Hello, ';
+let b = 'World';
+
+console.log(c)
+
+
+//// Stack trace error
+
+
+const aa = 5;
+const bb = 10;
+
+function add() {
+  return cc;
+}
+
+function print() {
+  add();
+}
+
+print();
+
+
+//// Syntax error
+
+
+function helloWorld() {
+    console.log "Hello World!"
+  }
+
+
+//// Type error
+
+const str1 = "Hello";
+const str2 = "World!";
+const message = str1.push(str2);
+
+
+
