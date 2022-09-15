420b695c7689a9e126dadb49f04bd8c10b341915
Philip Pretorius <philippretorius.79@gmail.com>
2022-09-15 15:39:25 +0200

    Array folder and files added

---
+++ arrays/array.js
@@ -0,0 +1,28 @@
+////// Arrays
+
+//// Create an array of car brands
+
+// Literal Method
+
+const germanCarBrands = ['Volkswagen', 'Mercedes', 'BMW', 'Opel', 'Audi']
+
+console.log(germanCarBrands[0]);
+
+// Literal Method with multiple lines 
+
+const swedishCarBrands = [
+    'Volvo', 
+    'Saab', 
+    'Scania'
+];
+
+console.log(swedishCarBrands[1]);
+
+// Literal Method with elements provided later
+
+const frenchCarBrands = [];
+frenchCarBrands [0] = 'Renault';
+frenchCarBrands [1] = 'Citroen';
+frenchCarBrands [2] = 'Peugeot';
+
+console.log(frenchCarBrands[2]);
