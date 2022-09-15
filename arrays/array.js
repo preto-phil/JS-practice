471a3fb77e1a214a380ae6820c83e457f987a445
Philip Pretorius <philippretorius.79@gmail.com>
2022-09-15 16:29:37 +0200

    Progress with Arrays

--- arrays/array.js
+++ arrays/array.js
@@ -4,7 +4,7 @@
 
 // Literal Method
 
-const germanCarBrands = ['Volkswagen', 'Mercedes', 'BMW', 'Opel', 'Audi']
+const germanCarBrands = ['Volkswagen', 'Mercedes', 'BMW', 'Opel']
 
 console.log(germanCarBrands[0]);
 
@@ -26,3 +26,60 @@ frenchCarBrands [1] = 'Citroen';
 frenchCarBrands [2] = 'Peugeot';
 
 console.log(frenchCarBrands[2]);
+
+//// Change element in array
+
+germanCarBrands [3] = 'Audi';
+
+console.log(germanCarBrands);
+
+//// Array properties and methods
+
+// Length
+
+let gcbLength = germanCarBrands.length;
+
+console.log(gcbLength);
+
+// Sort
+
+let scbSort = swedishCarBrands.sort();
+
+console.log(scbSort);
+
+//// Accessing arrays
+
+console.log(germanCarBrands[0]);
+console.log(frenchCarBrands[frenchCarBrands.length - 1]);
+
+//// Loops with Arrays
+
+// for.Each method
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+let text = "<ul>";
+fruits.forEach(myFunction);
+text += "</ul>";
+
+function myFunction(value) {
+text += "<li>" + value + "</li>";
+}
+
+document.getElementById("fruit").outerHTML = text;
+
+// for loop method
+
+const vegetables = ['Broccoli', 'Butternut', 'Spinach', 'Potatoes', 'Carrots'];
+
+let veglength = vegetables.length;
+
+let list = "<ul>";
+
+for (let i = 0; i < veglength; i++) {
+    list += "<li>" + vegetables[i] + "</li>";
+}
+
+list += "</ul>";
+
+document.getElementById("vegetables").outerHTML = list;
