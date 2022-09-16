6af7f04c46dc797a9b589faeebe40b0ae29c3b25
Philip Pretorius <philippretorius.79@gmail.com>
2022-09-16 17:49:18 +0200

    Loop article progress

---
+++ loops/loops.js
@@ -0,0 +1,47 @@
+////// Loops
+
+//// Simple looping through collection with a for...of loop
+
+const catArray = ['Lion', 'Tiger', 'Leopard', 'Cheetah', 'Caracal', 'Serval'];
+
+for (const cat of catArray) {
+    console.log(cat);
+}
+
+//// Using map()
+
+function toUpper(string) {
+    return string.toUpperCase();
+}
+
+const upperCats = catArray.map(toUpper);
+
+console.log(upperCats);
+
+//// filter()
+
+function lCat(cat) {
+    return cat.startsWith('L');
+}
+
+const filtered = catArray.filter(lCat);
+
+console.log(filtered);
+
+// With function expression
+
+const filtered2 = catArray.filter((cat) => cat.startsWith('L'));
+console.log(filtered2);
+
+// with function expression
+
+const upperCats2 = catArray.map((string) => string.toUpperCase());
+console.log(upperCats2);
+
+//// for loop
+
+for (i = 0; i < catArray.length ; i++ ) {
+    console.log(catArray[i]);
+}
+
+//// 
