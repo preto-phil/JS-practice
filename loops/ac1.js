d12032a10d6857442397b970333ec94619c8d084
Philip Pretorius <philippretorius.79@gmail.com>
2022-09-22 17:12:23 +0200

    Great success: Launch Countdown completed

--- loops/ac1.js
+++ loops/ac1.js
@@ -1,2 +1,19 @@
 //// Launch Countdown
 
+const output = document.querySelector('.output');
+
+for (let i = 10; i >= 0; i--) {
+    if (i === 10) {
+        const para = document.createElement('p');
+        output.appendChild(para);
+        para.textContent = `Countdown ${i}`;
+    } else if (i < 10 && i > 0) {
+        const para = document.createElement('p');
+        output.appendChild(para);
+        para.textContent = `${i}`;
+    } else {
+        const para = document.createElement('p');
+        output.appendChild(para);
+        para.textContent = 'Blast off!';
+    }
+}
