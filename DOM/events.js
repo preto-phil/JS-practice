8e38baada90c37d05fc520c5d829761c81bf2af4
Philip Pretorius <philippretorius.79@gmail.com>
2022-09-30 12:28:53 +0200

    Events completed

--- DOM/events.js
+++ DOM/events.js
@@ -1,1 +1,13 @@
-//// 
+//// Method 2 - on_event_
+
+const btn = document.querySelector('#btn');
+
+btn.onclick = () => alert('Hello, World!');
+
+//// Method 3 - Event Listeners
+
+const butt = document.querySelector('#butt');
+
+butt.addEventListener('click', () => {
+    alert('Hello, World!');
+});
