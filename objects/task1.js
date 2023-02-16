43d8c05e0b57751cff843f6bacfe9f6353b9f03a
Philip Pretorius <philippretorius.79@gmail.com>
2023-02-17 01:01:20 +0200

    Objects basics tutorial tasks finished

--- objects/task1.js
+++ objects/task1.js
@@ -1,3 +1,4 @@
+////// Task 1
 
 // Create empty object user
 let user = {};
@@ -14,3 +15,45 @@ user.name = "Pete";
 
 // Remove the property name from the object.
 delete user.name;
+
+
+////// Task 2
+
+// Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+function isEmpty(obj) {
+    for (let key in obj) {
+        // if the loop has started there is a property
+        return false;
+    }
+    return true;
+}
+
+
+////// Task 3
+
+// Sum Object properties
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+let sum = 0;
+for (let key in salaries) {
+    sum =+ salaries[key];
+}
+
+alert(sum);
+
+
+////// Task 4
+
+// Multiply numeric property values by 2
+
+function multiplyNumeric() {
+    for (let key in obj) {
+        if (typeof obj[key] == 'number') {
+            obj[key] * 2;
+        }
+    }
+}
