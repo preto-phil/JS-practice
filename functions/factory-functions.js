3ba116bd775fabb369ab12d87c214fab9c37c3d0
Philip Pretorius <philippretorius.79@gmail.com>
2023-04-22 13:45:12 +0200

    JS practice

--- functions/factory-functions.js
+++ functions/factory-functions.js
@@ -25,3 +25,87 @@ console.log(name, color, number, food); // Maynard red 34 rice
 // the output is much easier to decipher:
 console.log({name, color, number, food});
  // { name: 'Maynard', color: 'red', number: 34, food: 'rice' }
+
+
+// Using scope in game example
+
+const Player = (name, level) => {
+  let health = level * 2;
+  const getLevel = () => level;
+  const getName = () => name;
+  const die = () => {
+
+  };
+  const damage = x => {
+    health -= x;
+    if (health = 0) {
+      die();
+    }
+  };
+  const attack = enemy => {
+    if (level < enemy.getLevel()) {
+      damage(1);
+      console.log(`${name} has damaged ${enemy.getName()}`)
+    }
+    if (level >= enemy.getLevel()) {
+      enemy.damage(1);
+      console.log(`${name} has damaged ${enemy.getName()}`);
+    }
+  };
+  return { attack, damage, getLevel, getName };
+}
+
+
+// Inheritance 
+
+const jimmie = Player('jim', 10);
+const badGuy = Player('jeff', 5);
+jimmie.attack(badGuy);
+
+const Person = (name) => {
+  const sayName = () => console.log(`my name is ${name}`);
+  return {sayName};
+}
+
+const Nerd = (name) => {
+  // simply create a person and pull out the sayName function with destructuring assignment syntax!
+  const {sayName} = Person(name);
+  const doSomethingNerdy = () => console.log('nerd stuff');
+  return {sayName, doSomethingNerdy};
+}
+
+const joe = Nerd('joe');
+
+joe.sayName(); // my name is joe
+joe.doSomethingNerdy(); // nerd stuff
+
+// Using assign
+
+const Geek = (name) => {
+  const prototype = Person(name);
+  const doSomethingGeeky = () => console.log('Geek stuff');
+  return Object.assign({}, prototype, {doSomethingGeeky})
+}
+
+const jake = Geek('jake');
+
+jake.sayName();
+jake.doSomethingGeeky();
+
+// Module calculator
+const calculator = (() => {
+  const add = (a, b) => a + b;
+  const sub = (a, b) => a - b;
+  const mul = (a, b) => a * b;
+  const div = (a, b) => a / b;
+  return {
+    add,
+    sub,
+    mul,
+    div,
+  };
+})();
+
+calculator.add(3,5); // 8
+calculator.sub(6,2); // 4
+calculator.mul(14,5534); // 77476
