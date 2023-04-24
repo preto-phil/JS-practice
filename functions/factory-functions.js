3be8e094e73c84d511dfa1ab6d1c913f262521c3
Philip Pretorius <philippretorius.79@gmail.com>
2023-04-24 21:59:18 +0200

    Module patterns

--- functions/factory-functions.js
+++ functions/factory-functions.js
@@ -131,11 +131,34 @@ const myModule = (function() {
     publicMethod: function() {
       _privateMethod();
     }
-  }
-
+  };
 })();
 
 // Calling module content
 myModule.publicMethod();
 
+// Revealing module pattern
+const myRevealingModule = (function() {
+  'use strict';
+  
+  const _privateProperty = 'Hello World!';
+  const publicProperty = 'I am a public property!';
 
+  function _privateMethod() {
+    console.log(_privateProperty);
+  }
+
+  function publicMethod() {
+    _privateMethod();
+  }
+
+  return {
+    publicMethod: publicMethod,
+    publicProperty: publicProperty
+  };
+})();
+
+myModule.publicMethod(); // outputs 'Hello World'
+console.log(myModule.publicProperty); // outputs 'I am a public property'
+console.log(myModule._privateProperty); // is undefined protected by the module closure
+myModule._privateMethod(); // is TypeError protected by the module closure
