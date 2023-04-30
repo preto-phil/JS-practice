c8a89bed6c0d92af5a376d25768d729d7e665955
Philip Pretorius <philippretorius.79@gmail.com>
2023-04-30 21:31:52 +0200

    clock task finished

--- classes/clock.js
+++ classes/clock.js
@@ -1,3 +1,5 @@
+// Normal function version
+
 function Clock({ template }) {
 
   let timer;
@@ -36,8 +38,42 @@ function Clock({ template }) {
 let clock = new Clock({template: 'h:m:s'});
 clock.start();
 
+// Class version
+
 class Clock {
-  constructor() {
-    
+  constructor({template}) {
+    this.template = template;
+  }
+
+  render() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+
+    let secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+
+    let output = this.template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+
+    console.log(output);
+  }
+
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  start() {
+    render();
+    this.timer = setInterval(render, 1000);
   }
 }
+
+clock = new Clock({template: 'h.m.s'});
+clock.start();
