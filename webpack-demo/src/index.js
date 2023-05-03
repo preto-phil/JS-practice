86b13593d40aa9c30fefbc268738242dc510288c
Philip Pretorius <philippretorius.79@gmail.com>
2023-05-03 11:41:30 +0200

    Webpack Asset Loader tut progress

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,18 +1,42 @@
-// Default
+// Lodash
+import _ from 'lodash';
+// Default import
 import myName from './myName';
+// Named imports
+import {functionOne, functionTwo} from './myModule';
+// Style import
+import './style.css';
 
-// Named
-import {functionOne, functionTwo} from './myModule'
-
-function component() {
-  const element = document.createElement('div');
-  const element2 = document.createElement('div');
+function component1() {
+  const element1 = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.textContent = myName('Philip');
-  element2.textContent = functionOne() + functionTwo();
+  element1.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element1.classList.add('hello1');
 
-  return element;
+  return element1;
 }
 
-document.body.appendChild(component());
+function component2() {
+  const element2 = document.createElement('div');
+
+  element2.textContent = myName('Philip');
+  element2.classList.add('hello2');
+
+
+  return element2;
+}
+
+function component3() {
+  const element3 = document.createElement('div');
+
+  element3.textContent = functionOne() + functionTwo();
+  element3.classList.add('hello3');
+
+
+  return element3;
+}
+
+document.body.appendChild(component1());
+document.body.appendChild(component2());
+document.body.appendChild(component3());
