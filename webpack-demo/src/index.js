c26ffc1d6838d44d09ce740f94928377854813eb
Philip Pretorius <philippretorius.79@gmail.com>
2023-05-03 12:34:52 +0200

    Output mgmt tut progress

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,57 +1,18 @@
-// Lodash
 import _ from 'lodash';
-// Default import
-import myName from './myName';
-// Named imports
-import {functionOne, functionTwo} from './myModule';
-// Style import
-import './style.css';
-// Image import
-import Icon from './icon.png';
+import printMe from './print';
 
-function component1() {
-  const element1 = document.createElement('div');
+function component() {
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // Lodash, now imported by this script
-  element1.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element1.classList.add('hello1');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  return element1;
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild('btn');
+
+  return element;
 }
 
-function component2() {
-  const element2 = document.createElement('div');
-
-  element2.textContent = myName('Philip');
-  element2.classList.add('hello2');
-
-
-  return element2;
-}
-
-function component3() {
-  const element3 = document.createElement('div');
-
-  element3.textContent = functionOne() + functionTwo();
-  element3.classList.add('hello3');
-
-
-  return element3;
-}
-
-function addImage() {
-  const element4 = document.createElement('div');
-
-
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element4.appendChild(myIcon);
-
-  return element4
-}
-
-document.body.appendChild(component1());
-document.body.appendChild(component2());
-document.body.appendChild(component3());
-document.body.appendChild(addImage());
+document.body.appendChild(component());
