91f53d398f0f77167c6943698062586a56087937
Philip Pretorius <philippretorius.79@gmail.com>
2023-05-10 18:42:38 +0200

    Basic TDL tut finished

--- TDL/index.js
+++ TDL/index.js
@@ -17,4 +17,37 @@ for (let i = 0; i < close.length; i++) {
   }
 }
 
-// Add 
+// Add a 'checked' symbol when clicking on a list item
+const list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'li') {
+    ev.target.classList.add('checked');
+  }
+}, false)
+
+// Create a new list item when clicking on the 'Add' button
+function newElement() {
+  const li = document.createElement('li');
+  const inputValue = document.getElementById('myInput').value;
+  const t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  const span = document.createElement("SPAN");
+  const txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      const div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
