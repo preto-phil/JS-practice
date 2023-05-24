bc53cba9369b5a2bc9cb6340c63b263b0d0488fb
Philip Pretorius <philippretorius.79@gmail.com>
2023-05-24 13:33:10 +0200

    Image slider fin

--- DUII/Image-Slider/js.js
+++ DUII/Image-Slider/js.js
@@ -5,6 +5,15 @@ function setSlides(num) {
   displaySlides(currentIndex += num);
 }
 
+function timerSlide() {
+  setInterval(() => {
+    setSlides(1);
+  }, '5000')
+}
+
+timerSlide();
+
+
 function displaySlides(num) {
   let x;
   const slides = document.getElementsByClassName('imageSlides');
@@ -14,4 +23,10 @@ function displaySlides(num) {
     slides[x].style.display = 'none';
   }
   slides[currentIndex - 1].style.display = 'block';
+
+  const dots = document.getElementsByClassName("dot");
+  for (x = 0; x < dots.length; x++) {
+    dots[x].className = dots[x].className.replace(" active", "");
+  }
+  dots[currentIndex - 1].className += " active";
 }
