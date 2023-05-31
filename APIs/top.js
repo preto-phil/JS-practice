49530c778d72660c34aa40e2db1879d39294f234
Philip Pretorius <philippretorius.79@gmail.com>
2023-05-31 21:35:49 +0200

    API lesson complete

--- APIs/top.js
+++ APIs/top.js
@@ -1,13 +1,36 @@
-//// Fethcing data using fetch
+//// Giphy App
 
-fetch('https://api.weatherapi.com/v1/current.json?key=e0d3c7ebf93b4fb48c1121421233105&q=thabazimbi')
+// Declare img variable and link to img
+
+const img = document.querySelector('img');
+
+// Fetching data using fetch
+
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=mGWay0MRfopcreOrjHhrouhIc3lMAXlX&s=dogs', { mode: 'cors' })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    img.src = response.data.images.original.url;
+  });
+
+
+
+/* //// API form Giphy with key
+'https://api.giphy.com/v1/gifs/translate?api_key=YWyS1bNKM0DAJRUR9igAsKY3YlkK9BrPl&s=cats'
+
+
+
+
+
+// Weather App
+
+fetch('https://api.weatherapi.com/v1/current.json?key=e0d3c7ebf93b4fb48c1121421233105&q=thabazimbi', {
+  mode: 'cors',
+})
   .then(function(response) {
     // successful response
   })
   .catch(function(err) {
     // error
-  });
-
-
-//// API form Giphy with key
-'https://api.giphy.com/v1/gifs/translate?api_key=YWyS1bNKM0DAJRUR9igAsKY3YlkK9BrPlE&s=cats'
+  }); */
