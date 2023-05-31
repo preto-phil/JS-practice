66427a4bb89f01eb39b42faf8ac5c7436688993c
Philip Pretorius <philippretorius.79@gmail.com>
2023-05-31 14:46:03 +0200

    Session end: Fetch and HTML added

---
+++ APIs/top.js
@@ -0,0 +1,13 @@
+//// Fethcing data using fetch
+
+fetch('https://api.weatherapi.com/v1/current.json?key=e0d3c7ebf93b4fb48c1121421233105&q=thabazimbi')
+  .then(function(response) {
+    // successful response
+  })
+  .catch(function(err) {
+    // error
+  });
+
+
+//// API form Giphy with key
+'https://api.giphy.com/v1/gifs/translate?api_key=YWyS1bNKM0DAJRUR9igAsKY3YlkK9BrPlE&s=cats'
