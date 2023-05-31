5246742a3802fa8c438c627678da2546ac15bf80
Philip Pretorius <philippretorius.79@gmail.com>
2023-05-31 22:08:34 +0200

    Button added

--- APIs/top.js
+++ APIs/top.js
@@ -6,13 +6,19 @@ const img = document.querySelector('img');
 
 // Fetching data using fetch
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=mGWay0MRfopcreOrjHhrouhIc3lMAXlX&s=dogs', { mode: 'cors' })
+const newBtn = document.querySelector('button');
+newBtn.addEventListener('click', fetchGIF());
+
+function fetchGIF() {
+  fetch('https://api.giphy.com/v1/gifs/translate?api_key=mGWay0MRfopcreOrjHhrouhIc3lMAXlX&s=dogs', { mode: 'cors' })
   .then(function(response) {
     return response.json();
   })
   .then(function(response) {
     img.src = response.data.images.original.url;
   });
+}
+
 
 
 
