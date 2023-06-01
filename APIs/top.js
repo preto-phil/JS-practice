9b2c67c813b134c44060088fc3bf15e3cae59a14
Philip Pretorius <philippretorius.79@gmail.com>
2023-06-01 10:56:28 +0200

    Wholesale styling changes + some additions

--- APIs/top.js
+++ APIs/top.js
@@ -22,7 +22,14 @@ function fetchGIF() {
   });
 }
 
+function openGithub() {
+  const githubIcon = document.getElementById('github-icon');
+  githubIcon.addEventListener('click', () => {
+    window.open('https://github.com/preto-phil');
+  });
+}
 
+openGithub();
 
 
 /* //// API form Giphy with key
