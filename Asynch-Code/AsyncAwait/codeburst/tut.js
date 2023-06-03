d8b23a01cadd192ac2b8aeec34150e184095bc4c
Philip Pretorius <philippretorius.79@gmail.com>
2023-06-03 18:31:14 +0200

    Codeburst alterations

--- Asynch-Code/AsyncAwait/codeburst/tut.js
+++ Asynch-Code/AsyncAwait/codeburst/tut.js
@@ -31,5 +31,12 @@ async function addAsync(x) {
   const a = await doubleAfter2Seconds(10);
   const b = await doubleAfter2Seconds(20);
   const c = await doubleAfter2Seconds(30);
+  console.log(a)
+  console.log(b)
+  console.log(c)
   return x + a + b + c;
 }
+
+addAsync(10).then((sum) => {
+  console.log(sum)
+})
